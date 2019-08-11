aaf0316965fcfb4e7ee19ecc1cdc350296309477
Dan Kuehling <macprince@gmail.com>
2019-08-11 17:30:46 -0500

    Sorting non-dated logs by creation date
    The other half of the job.
    
    Also revised the regex for dated logs to match dates, not just digits.

--- sorter.rb
+++ sorter.rb
@@ -1,12 +1,27 @@
 #!/usr/bin/ruby
 require "ftools"
-Dir.chdir(File.expand_path("~/Documents/iChats"))
-Dir.new(File.expand_path("~/Documents/iChats")).each {
+require "date"
+
+iChatLogsDir = "~/Documents/iChats"
+Dir.chdir(File.expand_path(iChatLogsDir))
+Dir.new(File.expand_path(iChatLogsDir)).each {
   |x| 
     if File.ftype(x) != "directory"
-      if /(\d\d\d\d-\d\d-\d\d)/.match(x)
+      if /([12][09]\d\d-[01]\d-[0123]\d)/.match(x)
         File.makedirs $1
         File.move(x, $1)
       end
     end
 }
+puts "Dated logs finished."
+Dir.new(File.expand_path(iChatLogsDir)).each {
+  |y|
+    if File.ftype(y) != "directory" 
+      kMDcreationString = `/usr/bin/mdls -name kMDItemFSCreationDate "#{File.expand_path(y)}"`
+      creationDate = Date.parse(kMDcreationString)
+      File.makedirs creationDate.to_s
+      File.move(y, creationDate.to_s)
+    end
+}
+puts "Numbered logs finished."
+puts "Logs are now organized."

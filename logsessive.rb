9f5ed3305842edb9e5edbec4d0d55e8e65403877
Dan Kuehling <macprince@gmail.com>
2019-08-11 17:34:26 -0500

    Update logsessive.rb
    Timing how long the sorting takes
    Looking for the file extension ending in "chat" instead of just not being a directory.

--- logsessive.rb
+++ logsessive.rb
@@ -1,5 +1,8 @@
 #!/usr/bin/env ruby
 require "ftools"
+require "time"
+
+start = Time.now()
 
 iChatLogsDir = File.expand_path("~/Documents/iChats")
 
@@ -13,20 +16,23 @@ end
 Dir.chdir(iChatLogsDir)
 Dir.new(iChatLogsDir).each {
   |log| 
-    if File.ftype(log) != "directory"
-      if /([12][09]\d\d-[01]\d-[0123]\d)/.match(log)
+      if log.to_s.match(/chat$/)
+        puts log
+        /([12][09]\d\d-[01]\d-[0123]\d)/.match(log)
         moveLog(log, $1)
       end
-    end
 }
 puts "Dated logs finished."
 
 Dir.new(iChatLogsDir).each {
   |log|
-    if File.ftype(log) != "directory" 
-      /([12][09]\d\d-[01]\d-[0123]\d)/.match(`/usr/bin/mdls -name kMDItemFSCreationDate "#{File.expand_path(log)}"`)
-      moveLog(log, $1)
-    end
+      if log.to_s.match(/chat$/)
+        /([12][09]\d\d-[01]\d-[0123]\d)/.match(`/usr/bin/mdls -name kMDItemFSCreationDate "#{File.expand_path(log)}"`)
+        moveLog(log, $1)
+      end
 }
+timediff = Time.now() - start
+
 puts "Numbered logs finished."
-puts "Logs are now organized."
+puts "iChat logs are now organized."
+puts "Completed in #{timediff} seconds."
